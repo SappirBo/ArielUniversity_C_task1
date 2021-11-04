411175434594aceb5c884a34dc9a044c194f7447
SappirBo <92790326+SappirBo@users.noreply.github.com>
2021-11-04 12:20:10 +0200

    Add files via upload

--- basicClassification.c
+++ basicClassification.c
@@ -3,8 +3,8 @@
 
 int isPrime(int a)
 {
-    if(a==0||a==1){return 0;}
-    else if(a==2||a==3){return 1;}
+    if(a==0){return 0;}
+    else if(a==2||a==3||a==1){return 1;} // for this task, let 1 be a Prime number (as the tester commends).
     else
     {
     _Bool ans = 1;
